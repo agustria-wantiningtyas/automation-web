e26dc742f5e6430dee11acf35b2c9bddd20926eb
jejeabdulrahman <eljeje48@gmail.com>
2019-06-23 23:56:01 +0700

    jeje ganteng

--- src/app/theme/pages/default/setting/testCase/test-case.component.ts
+++ src/app/theme/pages/default/setting/testCase/test-case.component.ts
@@ -32,8 +32,8 @@ export class TestCaseComponent implements OnInit {
 
     // pagination
     public start_ta = 0;
-    public limit_ta = 5;
-    public rows = 5;
+    public limit_ta = 10;
+    public rows = 10;
     public pageLinks = 3;
     public rowsPerPageOptions: any = [5, 10, 20];
     public stop_ta = false;
